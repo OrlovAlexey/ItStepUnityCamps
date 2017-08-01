833a4ae158524c14fab25bb0e09250074466e14a
Unknown <vicnik83@gmail.com>
2017-08-01 16:44:51 +0300

    Update RollABall project

--- Roll a Ball/Assets/Scripts/CameraFollow.cs
+++ Roll a Ball/Assets/Scripts/CameraFollow.cs
@@ -1,24 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
 
-    [SerializeField]
-    private GameObject objectToFollow;
-    private Vector3 offset;
+	[SerializeField]
+	private GameObject objectToFollow;
+	[SerializeField]
+	private Vector3 cameraOffset = new Vector3 (0, 3, -3);
+	[SerializeField]
+	private Vector3 cameraSkew = new Vector3 (45, 0, 0);
 
 	private void Start ()
-    {
-        objectToFollow = GameObject.FindWithTag("Player");
-        offset = new Vector3 (0, 3, -3);
-        transform.eulerAngles = new Vector3 (45, 0, 0);
+	{
+		objectToFollow = GameObject.FindWithTag ("Player");
 	}
-	
+
 	private void LateUpdate ()
-    {
-        transform.position = objectToFollow.transform.position + offset;
+	{
+		transform.position = objectToFollow.transform.position + cameraOffset;
+		transform.eulerAngles = cameraSkew; //Better in start
 	}
 }
  
