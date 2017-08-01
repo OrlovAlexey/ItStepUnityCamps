56a0d2a01e88fd2c9de01a4783d04bb09c12d0ae
Unknown <vicnik83@gmail.com>
2017-08-01 10:43:42 +0300

    Update FlappyBird project
    Update readme.md

--- Flappy Bird/Assets/Scripts/RepeatingBackground.cs
+++ Flappy Bird/Assets/Scripts/RepeatingBackground.cs
@@ -4,20 +4,9 @@ public class RepeatingBackground : MonoBehaviour
 {
 
 	[SerializeField]
-	private BoxCollider2D groundCollider;
+	private float groundHorizontalLength = 1f;
 
-	private float groundHorizontalLength;
 
-	void Awake()
-	{
-		groundCollider = GetComponent<BoxCollider2D> ();
-	}
-
-	void Start ()
-	{
-		groundHorizontalLength = groundCollider.size.x;
-	}
-	
 	void Update ()
 	{
 		if (transform.position.x < -groundHorizontalLength)
