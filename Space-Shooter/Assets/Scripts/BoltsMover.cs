6f1258262f4bd01e1ca9b5e7a23369a8fbb8f81d
unknown <vicnik83@gmail.com>
2017-12-01 14:37:10 +0300

    Added LaserBolt

---
+++ Space-Shooter/Assets/Scripts/BoltsMover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoltsMover : MonoBehaviour
+{
+	[SerializeField]
+	private float speed;
+
+	//private GameObject parent;
+	private Rigidbody rb;
+
+	// Use this for initialization
+	private void Start()
+	{
+		//parent =
+		rb = GetComponent<Rigidbody>();
+		rb.velocity = transform.right * speed;
+	}
+}
