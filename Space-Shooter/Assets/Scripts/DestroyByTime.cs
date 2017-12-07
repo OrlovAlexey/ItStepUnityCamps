69f4e266db3800a5b35dfa549501d8b569cb9c42
unknown <vicnik83@gmail.com>
2017-12-07 10:17:25 +0300

    Added music & sound effects

--- Space-Shooter/Assets/Scripts/DestroyByTime.cs
+++ Space-Shooter/Assets/Scripts/DestroyByTime.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class DestroyByTime : MonoBehaviour
 {
-
+	[SerializeField]
+	private float alarm;
 	// Use this for initialization
 	void Start()
 	{
-		Destroy(gameObject, 5.0f);
+		Destroy(gameObject, alarm);
 	}
 }
--- Space-Shooter/Assets/Scripts/PlayerShooter.cs
+++ Space-Shooter/Assets/Scripts/PlayerShooter.cs
@@ -4,6 +4,7 @@ public class PlayerShooter : MonoBehaviour
 {
 	[SerializeField]
 	private GameObject bolt;
+	private AudioSource audioSource;
 	private Vector3 offset = new Vector3(0.8f, -0.08f, 0.0f);
 
 	private bool canShoot;
@@ -15,6 +16,7 @@ public class PlayerShooter : MonoBehaviour
 	private void Start()
 	{
 		canShoot = true;
+		audioSource = GetComponent<AudioSource>();
 	}
 
 	// Update is called once per frame
@@ -25,6 +27,7 @@ public class PlayerShooter : MonoBehaviour
 			if (Input.GetButton("Fire1"))
 			{
 				Instantiate(bolt, transform.position + offset, Quaternion.identity);
+				audioSource.Play();
 				canShoot = false;
 			}
 		}
