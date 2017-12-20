0df0486f3642834adbb5ff266dfadb304e3981e6
unknown <vicnik83@gmail.com>
2017-12-20 15:03:14 +0300

    Stars & scrolling background

---
+++ Space-Shooter/Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+
+	[SerializeField]
+	private float scrollSpeed;
+	private Vector3 startPosition;
+	private float tileSizeX;
+
+	// Use this for initialization
+	private void Start()
+	{
+		tileSizeX = transform.lossyScale.y;
+		Debug.Log(tileSizeX);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeX);
+		transform.position = startPosition + (Vector3.right * newPosition);
+	}
+}
