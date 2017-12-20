e9c24bad0f601409c772f11af6701665e716715b
unknown <vicnik83@gmail.com>
2017-12-20 11:15:18 +0300

    3 asteroids

--- Space-Shooter/Assets/Scripts/HazardsController.cs
+++ Space-Shooter/Assets/Scripts/HazardsController.cs
@@ -6,7 +6,7 @@ public class HazardsController : MonoBehaviour
 	private GameController gameController;
 
 	[SerializeField]
-	private GameObject hazard;
+	private GameObject[] hazards;
 	private Vector3 spawnPosition;
 	private int hazardsCount;
 	private float startWait;
@@ -19,7 +19,7 @@ public class HazardsController : MonoBehaviour
 	{
 		gameController = GameObject.FindWithTag("GameController").GetComponent<GameController>();
 
-		hazardsCount = 5;
+		hazardsCount = 10;
 		startWait = 1.0f;
 		spawnWait = 0.5f;
 		wavesStop = false;
@@ -33,12 +33,13 @@ public class HazardsController : MonoBehaviour
 		{
 			for (int i = 0; i < hazardsCount; i++)
 			{
+				GameObject hazard = hazards[Random.Range(0, hazards.Length)];
 				spawnPosition = new Vector3(10.0f, Random.Range(-4.5f, 4.5f), 0.0f);
 				Instantiate(hazard, spawnPosition, Quaternion.identity);
 				yield return new WaitForSeconds(spawnWait);
 
 			}
-			waveWait = hazardsCount * 0.5f;
+			waveWait = hazardsCount * spawnWait;
 			yield return new WaitForSeconds(waveWait);
 			hazardsCount++;
 			if (gameController.GetGameOver())
