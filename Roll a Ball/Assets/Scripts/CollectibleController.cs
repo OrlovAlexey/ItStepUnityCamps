d637e6e6947bbc2ed2023a39358d806a4be3b83b
Victor Nikitin <vicnik83@gmail.com>
2018-01-15 23:21:14 +0300

    Some changes in Roll a Ball

--- Roll a Ball/Assets/Scripts/CollectibleController.cs
+++ Roll a Ball/Assets/Scripts/CollectibleController.cs
@@ -3,46 +3,23 @@
 public class CollectibleController : MonoBehaviour
 {
 	[SerializeField]
-	private float collectibleMinimumVerticalSpeed = 0.5f;
-	[SerializeField]
-	private float collectibleMaximumVerticalSpeed = 1f;
-
-	private float collectibleVerticalSpeed;
-	private Vector3 collectibleMovement;
-
-	[SerializeField]
-	private float collectibleMinimumRotationSpeed = -60f;
-	[SerializeField]
-	private float collectibleMaximumRotationSpeed = 60f;
-
+	private float collectibleRotationSpeedLimit = 120f;
 	private float collectibleRotationSpeed;
 	private Vector3 collectibleRotation;
 
-	private void Awake ()
+	private void Awake()
 	{
-		collectibleVerticalSpeed = Random.Range (collectibleMinimumVerticalSpeed, collectibleMaximumVerticalSpeed);
-		collectibleRotationSpeed = Random.Range (collectibleMinimumRotationSpeed, collectibleMaximumRotationSpeed);
+		collectibleRotationSpeed = Random.Range(collectibleRotationSpeedLimit, -collectibleRotationSpeedLimit);
+
 	}
 
-	private void Start ()
+	private void Update()
 	{
-		transform.Rotate (45, 0, 35);
+		collectibleRotation = new Vector3(0f, collectibleRotationSpeed, 0f);
 	}
 
-	private void Update ()
+	private void FixedUpdate()
 	{
-		if (transform.position.y < 0f && collectibleVerticalSpeed < 0f ||
-		    transform.position.y > 0.6f && collectibleVerticalSpeed > 0f)
-		{
-			collectibleVerticalSpeed *= -1;
-		}
-		collectibleMovement = new Vector3 (0f, collectibleVerticalSpeed, 0f);
-		collectibleRotation = new Vector3 (0f, collectibleRotationSpeed, 0f);
-	}
-
-	private void FixedUpdate ()
-	{
-		transform.Translate (collectibleMovement * Time.deltaTime, Space.World);
-		transform.Rotate (collectibleRotation * Time.deltaTime, Space.World);
+		transform.Rotate(collectibleRotation * Time.deltaTime);
 	}
 }
